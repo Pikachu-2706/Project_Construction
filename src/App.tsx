cbd8bf040bfc46a544fbe2815475750d49f6973b
Pikachu-2706 <amhijatichekoli@gmail.com>
2025-08-17 14:29:52 +0530

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -77,6 +77,7 @@ const AppContent: React.FC = () => {
         <main className="flex-1 overflow-x-hidden overflow-y-auto bg-gray-50 p-6">
           {renderContent()}
         </main>
+        <Footer />
       </div>
     </div>
   );
