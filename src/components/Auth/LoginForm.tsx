de3ce00cb4a023e02b54383affb3c56e0a05a77f
Pikachu-2706 <amhijatichekoli@gmail.com>
2025-09-10 03:59:57 +0530

    Updated LoginForm.tsx

--- src/components/Auth/LoginForm.tsx
+++ src/components/Auth/LoginForm.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { Building, Eye, EyeOff, Lock, Mail } from 'lucide-react';
 import { useAuth } from '../../context/AuthContext';
-
+import { useNavigate } from 'react-router-dom';
 
 const LoginForm: React.FC = () => {
   const [formData, setFormData] = useState({
